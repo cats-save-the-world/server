2463a09169b749e7b7c43bfe079e7b4258d86271
RuslanKalashnikov <ruskalash100@gmail.com>
2023-01-10 18:35:36 +0600

    fix: fix checks in cat controller

--- code/controllers.py
+++ code/controllers.py
@@ -25,23 +25,23 @@ class CatController(Controller):
     def control(self, control_action_type: ControlActionTypes):
         self._degree += self._speed
 
-        if control_action_type == ControlActionTypes.LEFT:
-            if self._speed <= 0:
-                self._speed = max(self._speed - self.ACCELERATION_SPEED, -self.MAX_SPEED)
+        if control_action_type == ControlActionTypes.RIGHT:
+            if self._speed > 0:
+                self._speed = min(self._speed + self.ACCELERATION_SPEED, self.MAX_SPEED)
             else:
-                self._speed = min(self._speed - self.BRAKING_SPEED, self.MAX_SPEED)
+                self._speed += self.BRAKING_SPEED
 
-        elif control_action_type == ControlActionTypes.RIGHT:
-            if self._speed <= 0:
-                self._speed = min(self._speed + self.BRAKING_SPEED, self.MAX_SPEED)
+        elif control_action_type == ControlActionTypes.LEFT:
+            if self._speed > 0:
+                self._speed -= self.BRAKING_SPEED
             else:
-                self._speed = max(self._speed - self.ACCELERATION_SPEED, self.MAX_SPEED)
+                self._speed = max(self._speed - self.ACCELERATION_SPEED, -self.MAX_SPEED)
 
         elif control_action_type == ControlActionTypes.STOP:
-            if self._speed <= 0:
-                self._speed = min(self._speed + self.BRAKING_SPEED, 0)
-            else:
+            if self._speed > 0:
                 self._speed = max(self._speed - self.BRAKING_SPEED, 0)
+            else:
+                self._speed = min(self._speed + self.BRAKING_SPEED, 0)
 
 
 class GameController(Controller):
