ae6574c5c3f837a18be50780dd1044ec45fde34a
RuslanKalashnikov <ruskalash100@gmail.com>
2023-01-10 17:13:07 +0600

    feat: change cat control method

--- code/controllers.py
+++ code/controllers.py
@@ -12,6 +12,7 @@ class Controller(ABC):
 class CatController(Controller):
     ACCELERATION_SPEED: int = 2
     BRAKING_SPEED: int = 1
+    MAX_SPEED: int = 10
     _degree: int = 0
     _speed: int = 0
 
@@ -22,12 +23,25 @@ class CatController(Controller):
         }
 
     def control(self, control_action_type: ControlActionTypes):
+        self._degree += self._speed
+
         if control_action_type == ControlActionTypes.LEFT:
-            self._degree -= self.ACCELERATION_SPEED
+            if self._speed <= 0:
+                self._speed = max(self._speed - self.ACCELERATION_SPEED, -self.MAX_SPEED)
+            else:
+                self._speed = min(self._speed - self.BRAKING_SPEED, self.MAX_SPEED)
+
         elif control_action_type == ControlActionTypes.RIGHT:
-            self._degree += self.ACCELERATION_SPEED
+            if self._speed <= 0:
+                self._speed = min(self._speed + self.BRAKING_SPEED, self.MAX_SPEED)
+            else:
+                self._speed = max(self._speed - self.ACCELERATION_SPEED, self.MAX_SPEED)
+
         elif control_action_type == ControlActionTypes.STOP:
-            self._degree = 0
+            if self._speed <= 0:
+                self._speed = min(self._speed + self.BRAKING_SPEED, 0)
+            else:
+                self._speed = max(self._speed - self.BRAKING_SPEED, 0)
 
 
 class GameController(Controller):
