ce8bf25613842775bed98ede9bc3f4d01eb332eb
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-01-16 01:44:37 +0600

    add user model

--- code/models.py
+++ code/models.py
@@ -2,8 +2,22 @@ from tortoise import fields
 from tortoise.models import Model
 
 
+class User(Model):
+    id = fields.UUIDField(pk=True)  # noqa: A003
+    username = fields.CharField(max_length=20, unique=True)
+    password_hash = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table = 'users'
+
+
 class Game(Model):
     id = fields.UUIDField(pk=True)  # noqa: A003
+    user = fields.ForeignKeyField(
+        'models.User', related_name='games', null=True,
+    )  # type: ignore[var-annotated]
     is_active = fields.BooleanField(default=True)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
