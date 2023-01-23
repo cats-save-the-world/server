50c150aa849228d19697d9f6aa07d2acd2c84a9c
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-01-23 14:18:39 +0600

    add planet tests

--- tests/game/controllers/test_planet.py
+++ tests/game/controllers/test_planet.py
@@ -1,6 +1,7 @@
 import pytest
 
 from code.game.controllers.planet import PlanetController
+from code.game.exceptions import GameOver
 
 
 @pytest.mark.parametrize('health, damage', [
@@ -8,8 +9,17 @@ from code.game.controllers.planet import PlanetController
     (PlanetController.MAX_HEALTH / 2, 0.5),
     (0, 1),
 ])
-def test_damage(health: int, damage: int) -> None:
+def test_state(health: int, damage: int) -> None:
     controller = PlanetController()
     controller._health = health
 
     assert controller.state['damage'] == damage
+
+
+def test_get_damage_full() -> None:
+    controller = PlanetController()
+
+    with pytest.raises(GameOver):
+        controller.get_damage(PlanetController.MAX_HEALTH)
+
+    assert controller._health == 0
