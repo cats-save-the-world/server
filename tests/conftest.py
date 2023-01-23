45df371aecf8166ea904de181dd5edda832073b5
RuslanKalashnikov <ruskalash100@gmail.com>
2023-01-23 23:18:16 +0600

    fix: add prints

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,11 +12,13 @@ from code.config import TEST_TORTOISE_CONFIG
 async def database() -> AsyncGenerator:
     await Tortoise.init(config=TEST_TORTOISE_CONFIG, _create_db=True)
     await Tortoise.generate_schemas()
+    print(1)
     yield
     await Tortoise._drop_databases()
 
 
 @pytest.fixture
 async def client(database: None) -> AsyncGenerator:
-    async with AsyncClient(app=app, base_url='http://127.0.0.1') as async_client:
+    async with AsyncClient(app=app, base_url='http://test') as async_client:
         yield async_client
+    print(2)
