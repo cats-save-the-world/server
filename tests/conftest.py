0b182e437f492e7554b8a0605fe2b58afd52ff34
RuslanKalashnikov <ruskalash100@gmail.com>
2023-01-23 23:22:07 +0600

    fix: add noqa

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,7 @@ from code.config import TEST_TORTOISE_CONFIG
 async def database() -> AsyncGenerator:
     await Tortoise.init(config=TEST_TORTOISE_CONFIG, _create_db=True)
     await Tortoise.generate_schemas()
-    print(1)
+    print(1)  # noqa
     yield
     await Tortoise._drop_databases()
 
@@ -21,4 +21,4 @@ async def database() -> AsyncGenerator:
 async def client(database: None) -> AsyncGenerator:
     async with AsyncClient(app=app, base_url='http://test') as async_client:
         yield async_client
-    print(2)
+    print(2)  # noqa
