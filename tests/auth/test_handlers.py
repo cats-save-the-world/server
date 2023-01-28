134b847c6010b954944f0bfeb8be7e2ec2ca4f94
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-01-28 16:21:58 +0600

    add auth tests

--- tests/auth/test_handlers.py
+++ tests/auth/test_handlers.py
@@ -18,3 +18,25 @@ async def test_user_create_handler_failure(client: AsyncClient, user: User) -> N
         '/auth/users', json={'username': 'username', 'password': 'password'},
     )
     assert response.status_code == status.HTTP_409_CONFLICT
+
+
+async def test_verify_handler_success(client: AsyncClient, user: User) -> None:
+    response = await client.get('/auth/verify', auth=('username', 'password'))
+    assert response.status_code == status.HTTP_200_OK
+
+
+async def test_verify_handler_failure(client: AsyncClient) -> None:
+    response = await client.get('/auth/verify', auth=('username', 'password'))
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+async def test_username_exists_handler_success(client: AsyncClient, user: User) -> None:
+    response = await client.get('/auth/users', params={'username': 'username'})
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() is True
+
+
+async def test_username_exists_handler_failure(client: AsyncClient) -> None:
+    response = await client.get('/auth/users', params={'username': 'username'})
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() is False
