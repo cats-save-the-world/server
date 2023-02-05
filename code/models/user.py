694ef6870a225dc19f2f1274bcb7909a22edb485
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-02-05 14:35:29 +0600

    make models a package

---
+++ code/models/user.py
@@ -0,0 +1,13 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class User(Model):
+    id = fields.UUIDField(pk=True)  # noqa: A003
+    username = fields.CharField(max_length=20, unique=True)
+    password_hash = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table = 'users'
