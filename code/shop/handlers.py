b49db2b4b7721cad94336e0f55c592d6d7592e80
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-02-13 17:22:52 +0600

    fix buy skin handler

--- code/shop/handlers.py
+++ code/shop/handlers.py
@@ -1,7 +1,8 @@
 from uuid import UUID
 
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, Response, status
 from tortoise.exceptions import OperationalError
+from tortoise.expressions import F
 from tortoise.transactions import in_transaction
 
 from code.auth.dependencies import get_user
@@ -28,12 +29,14 @@ async def buy_skin_handler(  # type: ignore[no-untyped-def]
         async with in_transaction():
             user_skin, created = await UserSkin.get_or_create(user=user, skin=skin)
 
-            if not created:
-                user.balance -= skin.price
+            if created:
+                user.balance -= F('balance') - skin.price
                 await user.save(update_fields=['balance'])
                 await Transaction.create(
-                    user=user, reference_id=user_skin, amount=skin.price,
+                    user=user, reference_id=skin.id, amount=skin.price,
                     type=Transaction.Type.SKIN_PURCHASE,
                 )
     except OperationalError:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return Response(status_code=status.HTTP_200_OK)
--- code/shop/router.py
+++ code/shop/router.py
@@ -4,4 +4,4 @@ from code.shop import handlers
 
 router = APIRouter()
 router.add_api_route('/skins', handlers.get_skins_handler)
-router.add_api_route('/skins/{skin_id}/buy', handlers.buy_skin_handler)
+router.add_api_route('/skins/{skin_id}/buy', handlers.buy_skin_handler, methods=['post'])
